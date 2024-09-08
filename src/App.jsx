fb9e0d3b0bd8064e63202a094aef23980ee5232c
kaseracodes <shivamkasera0951@gmail.com>
2024-09-08 23:06:24 +0530

    added the bookings route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminPanel from "./AdminPanel";
+import BookingsList from "./BookingsList"
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<AdminPanel />} />
+        <Route path="/bookings" element={<BookingsList />} />
         {/* <Route path="/admin" element={<AdminPanel />} /> */}
       </Routes>
     </Router>
