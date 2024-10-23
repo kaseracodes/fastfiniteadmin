913e0b4379a653ea4188789f459e636ded765279
Tanmay Roy <tanmayroy84368@gmail.com>
2024-10-23 14:21:41 +0530

    change firebase project

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAM21ipI9MXcAv9AFQXZYheBPww7jqzi48",
-  authDomain: "fastfinite-bike-rental.firebaseapp.com",
-  projectId: "fastfinite-bike-rental",
-  storageBucket: "fastfinite-bike-rental.appspot.com",
-  messagingSenderId: "349020641736",
-  appId: "1:349020641736:web:6485a37a255d9158de48fd",
-  measurementId: "G-4V23PHP2BL",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
